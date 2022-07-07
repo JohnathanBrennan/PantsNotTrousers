31ffc7e27fc0b82d3e05f1da274582721fe37eb2
John Michael Brennan <john@johns-mbp.lan>
2022-07-06 21:19:41 -0500

    renamed index.css file to be sass, adjusted theme for header menu, reformatted to import header to app.js, updated dependencies. PLEASE RUN NPM INSTALL AND RESTART SERVER

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import './App.scss';
+import HeaderMenu from './components/HeaderMenu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+// $pnt_blue: #4a64b8;
+// $pnt_indigo: #2C4460;
+// $pnt_yellow: #F0F090;
+// $pnt_white: #F5E0F5;
+// $pnt_coffee: #81523F;
+
+
+export const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#2C4460',
+    },
+    secondary: {
+      main: '#4a64b8'
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Pants Not Trousers
-        </h1>
-        <p>
-          More than just a band. It's an idea.
-        </p>
-
-      </header>
+      <ThemeProvider theme={theme}>
+        <header className="App-header">
+          <HeaderMenu />
+          <h1>
+            Pants Not Trousers
+          </h1>
+          <p>
+            More than just a band. It's an idea.
+          </p>
+        </header>
+      </ThemeProvider>
     </div>
   );
 }

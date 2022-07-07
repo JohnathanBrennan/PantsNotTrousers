005f5b5690eac6f67eeaa8e6151a78e41dba7d2b
Brandon Daniels <brandaniels92@gmail.com>
2022-07-06 20:34:54 -0500

    Picked out color palette and fonts. Changed css to sass. Dependencies updated: run npm install

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Pants Not Trousers
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          More than just a band. It's an idea.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
